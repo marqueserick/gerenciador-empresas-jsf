e5e906d2b1cf75522d8cbe4d0bbc26503a13ba26
marqueserick <erickmarques43@gmail.com>
2023-04-27 17:05:23 -0300

    organiza formatacao

--- src/main/java/com/marqueserick/gerenciador/controller/EmpresaController.java
+++ src/main/java/com/marqueserick/gerenciador/controller/EmpresaController.java
@@ -13,22 +13,20 @@ import com.marqueserick.gerenciador.repository.EmpresaRepository;
 @Named("empresaController")
 @ViewScoped
 public class EmpresaController implements Serializable {
-	
-	
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EmpresaRepository repository;
-	
+
 	private List<Empresa> empresas;
-	
+
 	public void listar() {
 		empresas = repository.listar();
 	}
-	
+
 	public List<Empresa> getEmpresas() {
 		return empresas;
 	}
-	
+
 }
